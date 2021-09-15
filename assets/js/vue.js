a08f70b19818bc0728f202573afe7e420a647e9a
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-09-15 16:05:00 +0300

    Add files via upload

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,4 +1,82 @@
 // ici vue.js   ici on va faire intervenir tout notre vue
+
+// ici notre DB (prise de script.js)
+// data
+const products = [
+  {
+    id: 1,
+    description: "Quarz Luxe",
+    price: 12,
+    img: "assets/img/quarz-luxe.JPG",
+  },
+  {
+    id: 2,
+    description: "Curren Business",
+    price: 20,
+    img: "assets/img/curren-business.JPG",
+  },
+  {
+    id: 3,
+    description: "Curren Sport",
+    price: 5,
+    img: "assets/img/curren-sport.JPG",
+  },
+  {
+    id: 4,
+    description: "Jaragar Racing",
+    price: 8,
+    img: "assets/img/jaragar-racing.JPG",
+  },
+  {
+    id: 5,
+    description: "Liges Hommes",
+    price: 3,
+    img: "assets/img/liges-hommes.JPG",
+  },
+  {
+    id: 6,
+    description: "Maserati Mechanical",
+    price: 65,
+    img: "assets/img/maserati-mechanical.JPG",
+  },
+  {
+    id: 7,
+    description: "Montre Mecanique",
+    price: 25,
+    img: "assets/img/montre-mecanique.JPG",
+  },
+  {
+    id: 8,
+    description: "Brand Designer",
+    price: 28,
+    img: "assets/img/brand-designer.JPG",
+  },
+  {
+    id: 9,
+    description: "Relogio Masculino",
+    price: 4,
+    img: "assets/img/relogio-masculino.JPG",
+  },
+  {
+    id: 10,
+    description: "Tissot Multifunction",
+    price: 29,
+    img: "assets/img/tissot-multifunction.JPG",
+  },
+  {
+    id: 11,
+    description: "Hip Hop Gold",
+    price: 87,
+    img: "assets/img/hiphop-gold.JPG",
+  },
+  {
+    id: 12,
+    description: "Mesh Genova",
+    price: 6,
+    img: "assets/img/mesh-genova.JPG",
+  },
+];
+
 // on commence par créer une nouvelle variable: const vue = new Vue
 // dans l'instance de Vue il y a le router (c lui qui nous emmène vers les pages du site) on peut mettre plus d'info que juste le router
 // $mount("#app") veut dire on point se qui a été encadré dans la div : <div id="app"> for index.html
@@ -11,8 +89,17 @@
 // copier le liens (cdn) dans index.html
 
 const Home = {
-  template: "<h1>Ceci est la page d'accueil</h1>",
+  // template fait référence directement à #home (qui est sur le DOM ; voir index.html)
+  template: "#home",
   name: "Home",
+  // comment passer notre DB à notre component ; comme ça
+  // faut pas oublier le return ; car c une fct! pr travailler avec cette data, on passe au html
+  data: () => {
+    //   dans le JS modern: au lieu de products: products, on peut écrire directement: products
+    return {
+      products,
+    };
+  },
 };
 const UserSettings = {
   template: "<h1>Options utilisateur</h1>",
